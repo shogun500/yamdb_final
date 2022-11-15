d654a580384376246265a76ac32eb1ff353e79b1
shogun500 <trc@inbox.ru>
2022-11-16 00:20:49 +0300

    ver. 1

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,5 @@
-from django.urls import include, path
 from rest_framework import routers
-
+from django.urls import include, path
 from api.views import (CategoryViewSet, CommentViewSet, CreateTokenView,
                        CreateUserView, GenreViewSet, ReviewViewSet,
                        TitlesViewSet, UserMeView, UserViewSet)
--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-
 from reviews.models import Category, Genre, Title, Review, Comment
 
 User = get_user_model()
