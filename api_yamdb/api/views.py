8ab4086abf707eda68d1c2fe132fb48fdd9d22ee
shogun500 <trc@inbox.ru>
2022-11-16 00:25:25 +0300

    ver. 1

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -9,9 +9,8 @@ from rest_framework import filters, generics, permissions, status, viewsets
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-
-
 from reviews.models import Category, Genre, Title, User
+
 from api.permissions import (CommentRewiewPermission, IsAdminUserPermission,
                              ReadOnly)
 from api.serializer import (CategorySerializer, CommentSerializer,
